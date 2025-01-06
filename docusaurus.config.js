c25e0b6e2f7f60227567e490a06add3ab040a945
beliefandreason <beliefandreason.contact@gmail.com>
2025-01-06 19:13:59 +0100

    update intro

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -104,7 +104,7 @@ const config = {
             items: [
               {
                 label: 'Introduction',
-                to: '/docs/intro',
+                to: '/docs/introduction',
               },
             ],
           },
