d3f622d10edd348312d034cdebcfdb484ee10cc7
beliefandreason <beliefandreason.contact@gmail.com>
2025-01-06 23:18:07 +0100

    updated css and cleaned up header

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -89,11 +89,11 @@ const config = {
             label: 'Docs',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
+          //{
+            //href: 'https://github.com/facebook/docusaurus',
+            //label: 'GitHub',
+            //position: 'right',
+          //},
         ],
       },
       footer: {
--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,58 +5,61 @@ import styles from "./styles.module.css";
 
 const FeatureList = [
   {
-    title: "Spirit of the Living God",
-    Svg: require("@site/static/img/holy-spirit-fire.svg").default,
+    title: 'Spirit of the Living God',
+    Svg: require('@site/static/img/holy-spirit-fire.svg').default,
     description: {
-      text: "And you show that you are a letter from Christ delivered by us, written not with ink but with the Spirit of the living God, not on tablets of stone but on tablets of human hearts.",
-      reference: "2 Corinthians 3:3",
+      text: (
+        <>
+          And you show that you are a letter from Christ delivered by us, <b>written not with ink but with the Spirit of the living God</b>, not on tablets of stone but on tablets of human hearts.
+        </>
+      ),
+      reference: '2 Corinthians 3:3',
     },
   },
   {
-    title: "Living and Active",
-    Svg: require("@site/static/img/open-bible.svg").default,
+    title: 'Living and Active',
+    Svg: require('@site/static/img/open-bible.svg').default,
     description: {
-      text: "For the word of God is living and active. Sharper than any double-edged sword, it penetrates even to dividing soul and spirit, joints and marrow; it judges the thoughts and attitudes of the heart.",
-      reference: "Hebrews 4:12",
+      text: (
+        <>
+          For <b>the word of God is living and active</b>. Sharper than any double-edged sword, it penetrates even to dividing soul and spirit, joints and marrow; <b>it judges the thoughts and attitudes of the heart</b>.
+        </>
+      ),
+      reference: 'Hebrews 4:12',
     },
   },
   {
-    title: "Proclaim Yeshua! (יְשׁוּעָה)",
-    Svg: require("@site/static/img/mountains.svg").default,
+    title: 'Proclaim Yeshua! (יְשׁוּעָה)',
+    Svg: require('@site/static/img/mountains.svg').default,
     description: {
-      text: "How beautiful on the mountains are the feet of those who bring good news, who proclaim peace, who bring good tidings, who proclaim salvation, who say to Zion, “Your God reigns!”",
-      reference: "Isaiah 52:7",
+      text: (
+        <>
+          How beautiful on the mountains are the feet of those who bring good news, who proclaim peace, who bring good tidings, who proclaim <b>salvation</b>, who say to Zion, “<b>Your God reigns!</b>”
+        </>
+      ),
+      reference: 'Isaiah 52:7',
     },
   },
 ];
 
 function Feature({ Svg, title, description }) {
   return (
-    <div className={clsx("col col--4")}>
+    <div className={clsx('col col--4')}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
       <div className="text--center padding-horiz--md">
-        <Heading
-          as="h3"
-          style={{
-            color: `var(--feature-heading-color, #10a6d9)`,
+        <Heading 
+          as="h3" 
+          style={{ 
+            color: `var(--feature-heading-color, #10a6d9)` 
           }}
         >
           {title}
         </Heading>
         <div>
-          {/* General description text */}
-          <p style={{ color: "inherit", fontWeight: "normal" }}>
-            {description.text}
-          </p>
-          {/* Bible reference styling */}
-          <p
-            style={{
-              color: `var(--bible-ref-color, #2e4885)`,
-              fontWeight: `var(--bible-ref-font-weight, bold)`,
-            }}
-          >
+          <p>{description.text}</p>
+          <p style={{ color: `var(--bible-ref-color, #59ccf3)`, fontWeight: `var(--bible-ref-font-weight, bold)` }}>
             {description.reference}
           </p>
         </div>
